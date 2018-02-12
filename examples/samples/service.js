5667f908ef156525f467aa28e521bc3815d36694
akito hataguchi <hataguchi@motionpicture.jp>
2018-02-12 20:38:58 +0900

    MWSR-148 ムビチケサービス3.0

--- examples/samples/service.js
+++ examples/samples/service.js
@@ -1,16 +1,16 @@
 const mvtk = require('../../lib/index');
 
-mvtk.services.initialize(
+mvtk.service.initialize(
     'https://ssl.movieticket.jp',
-    'https://testservices.movieticket.jp'
+    'https://test service.movieticket.jp'
 );
 
 main();
 
 async function main() {
     try {
-        const filmService = mvtk.services.createFilmService();
-        const filmTopPage = await filmService.getFilmTopPage(mvtk.services.Constants.DVC_TYP_PC);
+        const filmService = mvtk. service.createFilmService();
+        const filmTopPage = await filmService.getFilmTopPage(mvtk. service.Constants.DVC_TYP_PC);
         console.log(filmTopPage.result);
     } catch (err) {
         console.log(err);
