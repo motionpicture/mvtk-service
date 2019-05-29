55fa9c475efdb8777d46118753d1e19790dce583
h.matsuoka <h.matsuoka@motionpicture.jp>
2019-05-29 15:16:03 +0900

    xmlを一部stringに戻す

--- src/services/giftCard/models/MvtkGiftCardEntryIn.ts
+++ src/services/giftCard/models/MvtkGiftCardEntryIn.ts
@@ -34,47 +34,48 @@ export class MvtkGiftCardEntryIn extends BaseIn {
 
     public toXml(): any {
         // パラメータの順序が異なるとエラーになるので注意
-        return {
-            MVTKGFTCRD_INFO_IN: {
-                MvtkgftcrdInfoIn: this.MVTKGFTCRD_INFO_IN.MvtkgftcrdInfoIn.map((info) => {
-                    return {
-                        GFTCRDKSSIKNR_NO: info.GFTCRDKSSIKNR_NO,
-                        MVTKGFTCRDPIN_CD: info.MVTKGFTCRDPIN_CD,
-                        MVTKGFTCRD_ID: info.MVTKGFTCRD_ID,
-                        targetNSAlias: 'tns',
-                        // tslint:disable-next-line:no-http-string max-line-length
-                        targetNamespace: 'http://schemas.datacontract.org/2004/07/MTES.WCFModel.MvtkGiftCard.MvtkGiftCardSvc.MvtkGiftCardIDAuthModel'
-                    };
-                }),
-                targetNSAlias: 'q2',
-                // tslint:disable-next-line:no-http-string max-line-length
-                targetNamespace: 'http://schemas.datacontract.org/2004/07/MTES.WCFModel.MvtkGiftCard.MvtkGiftCardSvc.MvtkGiftCardIDAuthModel'
-            },
-            DVC_TYP: this.DVC_TYP
-        };
+        // return {
+        //     MVTKGFTCRD_INFO_IN: {
+        //         MvtkgftcrdInfoIn: this.MVTKGFTCRD_INFO_IN.MvtkgftcrdInfoIn.map((info) => {
+        //             return {
+        //                 GFTCRDKSSIKNR_NO: info.GFTCRDKSSIKNR_NO,
+        //                 MVTKGFTCRDPIN_CD: info.MVTKGFTCRDPIN_CD,
+        //                 MVTKGFTCRD_ID: info.MVTKGFTCRD_ID,
+        //                 RYUKNGK: info.RYUKNGK,
+        //                 targetNSAlias: 'tns',
+        //                 // tslint:disable-next-line:no-http-string max-line-length
+        //                 targetNamespace: 'http://schemas.datacontract.org/2004/07/MTES.WCFModel.MvtkGiftCard.MvtkGiftCardSvc.MvtkGiftCardEntryModel'
+        //             };
+        //         }),
+        //         targetNSAlias: 'q4',
+        //         // tslint:disable-next-line:no-http-string max-line-length
+        //         targetNamespace: 'http://schemas.datacontract.org/2004/07/MTES.WCFModel.MvtkGiftCard.MvtkGiftCardSvc.MvtkGiftCardEntryModel'
+        //     },
+        //     DVC_TYP: this.DVC_TYP
+        // };
 
-        //         // パラメータの順序が異なるとエラーになるので注意
-        //         let message = `
-        // <tns:MvtkGiftCardEntry>
-        //     <tns:MVTKGFTCRD_INFO_IN>
-        // `;
-        //         for (const info of this.MVTKGFTCRD_INFO_IN.MvtkgftcrdInfoIn) {
-        //             message += `
-        //         <q4:MvtkgftcrdInfoIn>
-        //             <q4:GFTCRDKSSIKNR_NO>${info.GFTCRDKSSIKNR_NO}</q4:GFTCRDKSSIKNR_NO>
-        //             <q4:MVTKGFTCRDPIN_CD>${info.MVTKGFTCRDPIN_CD}</q4:MVTKGFTCRDPIN_CD>
-        //             <q4:MVTKGFTCRD_ID>${info.MVTKGFTCRD_ID}</q4:MVTKGFTCRD_ID>
-        //             <q4:RYUKNGK>${info.RYUKNGK}</q4:RYUKNGK>
-        //         </q4:MvtkgftcrdInfoIn>
-        // `;
-        //         }
+        // パラメータの順序が異なるとエラーになるので注意
+        let message = `
+        <tns:MvtkGiftCardEntry>
+            <tns:MVTKGFTCRD_INFO_IN>
+        `;
+        for (const info of this.MVTKGFTCRD_INFO_IN.MvtkgftcrdInfoIn) {
+            message += `
+                <q4:MvtkgftcrdInfoIn>
+                    <q4:GFTCRDKSSIKNR_NO>${info.GFTCRDKSSIKNR_NO}</q4:GFTCRDKSSIKNR_NO>
+                    <q4:MVTKGFTCRDPIN_CD>${info.MVTKGFTCRDPIN_CD}</q4:MVTKGFTCRDPIN_CD>
+                    <q4:MVTKGFTCRD_ID>${info.MVTKGFTCRD_ID}</q4:MVTKGFTCRD_ID>
+                    <q4:RYUKNGK>${info.RYUKNGK}</q4:RYUKNGK>
+                </q4:MvtkgftcrdInfoIn>
+        `;
+        }
 
-        //         message += `
-        //     </tns:MVTKGFTCRD_INFO_IN>
-        //     <tns:DVC_TYP>${this.DVC_TYP}</tns:DVC_TYP>
-        // </tns:MvtkGiftCardEntry>
-        // `;
+        message += `
+            </tns:MVTKGFTCRD_INFO_IN>
+            <tns:DVC_TYP>${this.DVC_TYP}</tns:DVC_TYP>
+        </tns:MvtkGiftCardEntry>
+        `;
 
-        //         return message;
+        return { _xml: message };
     }
 }
--- src/services/webMoney/models/WebMoneyEntryIn.ts
+++ src/services/webMoney/models/WebMoneyEntryIn.ts
@@ -20,56 +20,56 @@ export class WebMoneyEntryIn extends BaseIn {
 
     public toXml(): any {
         // パラメータの順序が異なるとエラーになるので注意
-        return {
-            kssiknrNo: this.kssiknrNo,
-            skhnNm: this.skhnNm,
-            dvcTyp: this.dvcTyp,
-            knshknknrNo: this.knshknknrNo,
-            knshknInfo: {
-                KnshInfoIn: this.knshknInfo.KnshInfoIn.map((info) => {
-                    return {
-                        KNSHKBN_NM: info.KNSHKBN_NM,
-                        KNSHKNHMBI_UNIP: info.KNSHKNHMBI_UNIP,
-                        KNSHKNKNRMISI_NO: info.KNSHKNKNRMISI_NO,
-                        KNYMI_NUM: info.KNYMI_NUM,
-                        targetNSAlias: 'tns',
-                        // tslint:disable-next-line:no-http-string max-line-length
-                        targetNamespace: 'http://schemas.datacontract.org/2004/07/MTES.WCFModel.WebMoney.WebMoneySvc.WebMoneyEntry'
-                    };
-                }),
-                targetNSAlias: 'q1',
-                // tslint:disable-next-line:no-http-string max-line-length
-                targetNamespace: 'http://schemas.datacontract.org/2004/07/MTES.WCFModel.WebMoney.WebMoneySvc.WebMoneyEntry'
-            },
-            rykngk: this.rykngk
-        };
+        // return {
+        //     kssiknrNo: this.kssiknrNo,
+        //     skhnNm: this.skhnNm,
+        //     dvcTyp: this.dvcTyp,
+        //     knshknknrNo: this.knshknknrNo,
+        //     knshknInfo: {
+        //         KnshInfoIn: this.knshknInfo.KnshInfoIn.map((info) => {
+        //             return {
+        //                 KNSHKBN_NM: info.KNSHKBN_NM,
+        //                 KNSHKNHMBI_UNIP: info.KNSHKNHMBI_UNIP,
+        //                 KNSHKNKNRMISI_NO: info.KNSHKNKNRMISI_NO,
+        //                 KNYMI_NUM: info.KNYMI_NUM,
+        //                 targetNSAlias: 'tns',
+        //                 // tslint:disable-next-line:no-http-string max-line-length
+        //                 targetNamespace: 'http://schemas.datacontract.org/2004/07/MTES.WCFModel.WebMoney.WebMoneySvc.WebMoneyEntry'
+        //             };
+        //         }),
+        //         targetNSAlias: 'q1',
+        //         // tslint:disable-next-line:no-http-string max-line-length
+        //         targetNamespace: 'http://schemas.datacontract.org/2004/07/MTES.WCFModel.WebMoney.WebMoneySvc.WebMoneyEntry'
+        //     },
+        //     rykngk: this.rykngk
+        // };
 
-        //         let message = `
-        // <tns:WebMoneyEntry>
-        //     <tns:kssiknrNo>${this.kssiknrNo}</tns:kssiknrNo>
-        //     <tns:skhnNm>${this.skhnNm}</tns:skhnNm>
-        //     <tns:dvcTyp>${this.dvcTyp}</tns:dvcTyp>
-        //     <tns:knshknknrNo>${this.knshknknrNo}</tns:knshknknrNo>
-        //     <tns:knshknInfo>
-        // `;
+                let message = `
+        <tns:WebMoneyEntry>
+            <tns:kssiknrNo>${this.kssiknrNo}</tns:kssiknrNo>
+            <tns:skhnNm>${this.skhnNm}</tns:skhnNm>
+            <tns:dvcTyp>${this.dvcTyp}</tns:dvcTyp>
+            <tns:knshknknrNo>${this.knshknknrNo}</tns:knshknknrNo>
+            <tns:knshknInfo>
+        `;
 
-        //         for (const info of this.knshknInfo.KnshInfoIn) {
-        //             message += `
-        //         <q1:KnshInfoIn>
-        //             <q1:KNSHKBN_NM>${info.KNSHKBN_NM}</q1:KNSHKBN_NM>
-        //             <q1:KNSHKNHMBI_UNIP>${Math.floor(parseInt(info.KNSHKNHMBI_UNIP, 10)).toString()}</q1:KNSHKNHMBI_UNIP>
-        //             <q1:KNSHKNKNRMISI_NO>${info.KNSHKNKNRMISI_NO}</q1:KNSHKNKNRMISI_NO>
-        //             <q1:KNYMI_NUM>${info.KNYMI_NUM}</q1:KNYMI_NUM>
-        //         </q1:KnshInfoIn>
-        // `;
-        //         }
+                for (const info of this.knshknInfo.KnshInfoIn) {
+                    message += `
+                <q1:KnshInfoIn>
+                    <q1:KNSHKBN_NM>${info.KNSHKBN_NM}</q1:KNSHKBN_NM>
+                    <q1:KNSHKNHMBI_UNIP>${Math.floor(parseInt(info.KNSHKNHMBI_UNIP, 10)).toString()}</q1:KNSHKNHMBI_UNIP>
+                    <q1:KNSHKNKNRMISI_NO>${info.KNSHKNKNRMISI_NO}</q1:KNSHKNKNRMISI_NO>
+                    <q1:KNYMI_NUM>${info.KNYMI_NUM}</q1:KNYMI_NUM>
+                </q1:KnshInfoIn>
+        `;
+                }
 
-        //         message += `
-        //     </tns:knshknInfo>
-        //     <tns:rykngk>${this.rykngk}</tns:rykngk>
-        // </tns:WebMoneyEntry>
-        // `;
+                message += `
+            </tns:knshknInfo>
+            <tns:rykngk>${this.rykngk}</tns:rykngk>
+        </tns:WebMoneyEntry>
+        `;
 
-        //         return message;
+        return { _xml: message };
     }
 }
